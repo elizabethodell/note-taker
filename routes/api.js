4a0a6903fd7edeb807a5fc5d5bfe733d08d0a550
ebo16@vt.edu <ebo16@vt.edu>
2021-11-13 18:39:50 -0500

    added routes for html and apis

---
+++ routes/api.js
@@ -0,0 +1,58 @@
+const fs = require("fs")
+const path = require("path")
+const notesArray = require("../Develop/db/db.json");
+const router = require('express').Router();
+
+
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './Develop/db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+
+router.get('/notes', (req, res) => {
+    res.json(notesArray.slice(1));
+});
+
+router.post('/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notesArray);
+    res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notesArray);
+    res.json(true);
+});
+
+module.exports = router;
